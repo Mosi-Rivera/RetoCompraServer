47c7f65ab8012aef30e346b297a9a211cf2c3d4f
Mosi-Rivera <mosiriveraserra@gmail.com>
2024-01-26 04:51:24 -0400

    adds: text search route.

--- routes/product_routes.js
+++ routes/product_routes.js
@@ -1,7 +1,10 @@
-const { getProducts } = require('../controllers/product_controllers');
+const { getProducts, searchProducts, getInfo } = require('../controllers/product_controllers');
 
 const router = require('express').Router();
 
 router.route('/').get(getProducts);
+
+router.route('/search/:search').get(searchProducts);
+
 
 module.exports = router;
