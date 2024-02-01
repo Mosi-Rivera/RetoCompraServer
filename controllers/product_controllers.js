ee305b60662bb052870dd7557e7f19a24367da82
Mosi-Rivera <mosiriveraserra@gmail.com>
2024-02-01 18:26:41 -0400

    Adds: Query documents (count / limit) to facilitate pagination.

--- controllers/product_controllers.js
+++ controllers/product_controllers.js
@@ -6,15 +6,18 @@ _exports.getProducts =  async (req, res, next) => {
     try
     {
         const [query, skip, limit, sort] = Variant.parseQuery(req.query);
-        const products = await Variant.find(query).sort(sort || {}).skip(skip).limit(limit).select({
-            _id: 1,
-            product: 1,
-            'assets.thumbnail': 1,
-            name: 1,
-            brand: 1,
-            price: 1
-        });
-        res.status(200).json(products);
+        const [products, count] = await Promise.all([
+            Variant.find(query).sort(sort || {}).skip(skip).limit(limit).select({
+                _id: 1,
+                product: 1,
+                'assets.thumbnail': 1,
+                name: 1,
+                brand: 1,
+                price: 1
+            }),
+            Variant.countDocuments(query)
+        ]);
+        res.status(200).json({products, pages: Math.ceil(count / limit)});
     }
     catch(err)
     {
@@ -28,24 +31,27 @@ _exports.searchProducts = async (req, res, next) => {
         const [query, skip, limit, sort] = Variant.parseQuery(req.query);
         const search = req.params.search;
         const regex = { $regex: new RegExp(`.*${search}.*`, 'i') };
-        const products = await Variant.find({
-            $or: [
-                {name: regex},
-                {brand: regex},
-                {section: regex},
-                {color: regex}
-            ],
-            ...query
-        }).sort(sort || {}).skip(skip).limit(limit).select({
-            _id: 1,
-            product: 1,
-            'assets.thumbnail': 1,
-            name: 1,
-            brand: 1,
-            price: 1,
-            color: 1
-        });
-        res.status(200).json(products);
+        const [products, count] = await Promise.all([
+            Variant.find({
+                $or: [
+                    {name: regex},
+                    {brand: regex},
+                    {section: regex},
+                    {color: regex}
+                ],
+                ...query
+            }).sort(sort || {}).skip(skip).limit(limit).select({
+                _id: 1,
+                product: 1,
+                'assets.thumbnail': 1,
+                name: 1,
+                brand: 1,
+                price: 1,
+                color: 1
+            }),
+            Variant.countDocuments(query)
+        ]);
+        res.status(200).json({products, pages: Math.ceil(count / limit)});
     }
     catch(err)
     {
