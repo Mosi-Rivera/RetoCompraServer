15174edb1de9ffd4f2aec7d7861736785a28a1dc
Mosi-Rivera <mosiriveraserra@gmail.com>
2024-04-23 17:04:32 -0400

    Adds: Role and token validation to user routes.

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,11 +1,14 @@
 const { getUsers, setUserRole } = require('../controllers/user_controllers');
+const { authenticateToken } = require('../middlewares/authorization');
+const { validateRole } = require('../middlewares/validateRole');
+const {STAFF, ADMIN} = require('../constants/role').obj;
 
 const router = require('express').Router();
 
 //TODO: ADD routed guards;
 
-router.route('/').get(getUsers);
+router.route('/').get(authenticateToken, validateRole([STAFF, ADMIN]), getUsers);
 
-router.route('/role').patch(setUserRole);
+router.route('/role').patch(authenticateToken, validateRole([STAFF, ADMIN]), setUserRole);
 
 module.exports = router;
